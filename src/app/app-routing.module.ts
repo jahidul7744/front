4d4dc31a4ac93491d9e4740159c20bae09410598
Jahidul Islam <islam.jahidul@egeneration.co>
2025-03-12 14:30:56 +0600

    component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomerListComponent } from './customer/customer-list/customer-list.component';
+import { CustomerAddComponent } from './customer/customer-add/customer-add.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'customer-add', component: CustomerAddComponent },
+  { path: '', component: CustomerListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
